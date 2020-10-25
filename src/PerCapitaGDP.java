39dd5f02ea5c40f38abc1d548790379c535fa0e0
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2020-10-24 20:42:16 -0400

    Create GdpChartViewController, GdpChartView.fxml, SceneChanger utility class, and Main class.

--- src/PerCapitaGDP.java
+++ src/PerCapitaGDP.java
@@ -1,3 +1,45 @@
 public class PerCapitaGDP {
 
+    public String countryName, countryCode;
+    public int year;
+    public double gdp;
+
+    public PerCapitaGDP(String countryName, String countryCode, int year, double gdp) {
+        setCountryName(countryName);
+        setCountryCode(countryCode);
+        setYear(year);
+        setGdp(gdp);
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getGdp() {
+        return gdp;
+    }
+
+    public void setGdp(double gdp) {
+        this.gdp = gdp;
+    }
 }
